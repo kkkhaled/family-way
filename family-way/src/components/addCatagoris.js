589d68cc91eca7c46f7136571006531c1b52a377
OmarYuusf <omar.atef.mohsen@gmail.com>
2021-01-17 14:24:15 +0200

    edit style in products & categories

--- family-way/src/components/addCatagoris.js
+++ family-way/src/components/addCatagoris.js
@@ -9,11 +9,11 @@ import {
   Dialog,
   DialogActions,
   DialogContent,
-  DialogTitle,
+  DialogTitle
 } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
 import DeleteForeverIcon from '@material-ui/icons/DeleteForever'
-import { Alert } from "@material-ui/lab";
+import { Alert } from '@material-ui/lab'
 //import EditIcon from '@material-ui/icons/Edit'
 import { authContext } from '../contexts/auth/authstate'
 import { catagoriesContext } from '../contexts/catagories/catagoriesState'
@@ -21,31 +21,19 @@ import Animations from './loader'
 import { Switch } from '@material-ui/core'
 import FormControlLabel from '@material-ui/core/FormControlLabel'
 import EditIcon from '@material-ui/icons/Edit'
-import { url } from '../constants/constants';
-import axios from 'axios';
+import { url } from '../constants/constants'
+import axios from 'axios'
 
 const useStyles = makeStyles(theme => ({
   button: {
-    width: '22em',
+    width: '100%',
     color: 'white',
-    border: 5,
     marginTop: '8px',
     marginBottom: '15px',
     backgroundColor: theme.palette.green.main
   },
-
   input: {
-    marginLeft: '10px',
-    marginRight: '10px',
-    width: '70em',
-    marginTop: '40px'
-  },
-  input2: {
-    marginLeft: '10px',
-    marginRight: '10px',
-    width: '70em',
-    marginBottom: '10px',
-    marginTop: '20px'
+    flex: '1'
   },
   dialog: {
     width: '100% !important'
@@ -53,7 +41,7 @@ const useStyles = makeStyles(theme => ({
   card: {
     margin: '8px',
     width: '260px',
-    padding: "6px"
+    padding: '6px'
   },
   name: {
     marginLeft: '10px',
@@ -62,23 +50,34 @@ const useStyles = makeStyles(theme => ({
   editicon: {
     backgroundColor: theme.palette.yellow.main,
     color: 'white',
-    marginLeft: "5px"
-    //marginRight: "5px",
-    //marginTop: "5px",
+    marginLeft: '5px',
+    cursor: 'pointer',
+    borderRadius: '50%',
+    padding: 2
   },
   delIcon: {
     color: 'white',
-    marginLeft: "5px",
-    backgroundColor: theme.palette.secondary.main
-    //marginRight :"5px"
+    marginLeft: '5px',
+    backgroundColor: theme.palette.secondary.main,
+    cursor: 'pointer',
+    borderRadius: '50%',
+    padding: 2
+  },
+  cardOfCategories: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    border: '1px solid rgba(0,0,0,.15)',
+    borderRadius: 3,
+    padding: 10
   }
 }))
 const AddCatagiories = () => {
-  const classes = useStyles();
-  const [name, setName] = useState('');
-  const [sort, setSort] = useState('');
-  const [isCompany, setIsCompany] = useState(false);
-  const [alertData, setAlertData] = useState({ open: false });
+  const classes = useStyles()
+  const [name, setName] = useState('')
+  const [sort, setSort] = useState('')
+  const [isCompany, setIsCompany] = useState(false)
+  const [alertData, setAlertData] = useState({ open: false })
   const { loadUser } = useContext(authContext)
   const [openDialog, setOpenDialog] = useState(false)
   const [editInput, setEditInput] = useState('')
@@ -111,18 +110,17 @@ const AddCatagiories = () => {
     if (name === '' || sort === '') {
       setAlertData({
         open: true,
-        message: "تاكد من ادخال البيانات بشكل صحيح",
-        type: "error",
-      });
+        message: 'تاكد من ادخال البيانات بشكل صحيح',
+        type: 'error'
+      })
     } else {
-      addNewCategories(name, sort, isCompany);
+      addNewCategories(name, sort, isCompany)
       setAlertData({
         open: true,
-        message: "تم اضافه الصنف  ",
-        type: "success"
-      });
+        message: 'تم اضافه الصنف  ',
+        type: 'success'
+      })
     }
-
   }
 
   const handleUpdate = async () => {
@@ -134,16 +132,17 @@ const AddCatagiories = () => {
       console.log(response)
       setAlertData({
         open: true,
-        message: "تم اضافه الصنف",
-        type: "success"
-      });
+        message: 'تم اضافه الصنف',
+        type: 'success'
+      })
+      getAllCatagories()
     } catch (error) {
       console.log(error)
       setAlertData({
         open: true,
         message: error.message,
-        type: "error"
-      });
+        type: 'error'
+      })
     }
   }
 
@@ -153,57 +152,49 @@ const AddCatagiories = () => {
         <Alert severity={alertData.type}>{alertData.message}</Alert>
       ) : null}
       <form onSubmit={handleSubmit}>
-        <Grid container direction='column'>
-          <Grid item>
+        <Grid container style={{ gridGap: '10px' }}>
+          <Grid item style={{ flex: 1 }}>
             <TextField
-              className={classes.input}
+              style={{ width: '100%' }}
               variant='outlined'
               label='ادخال اسم الصنف'
               onChange={e => setName(e.target.value)}
             />
           </Grid>
-          <Grid item>
+          <Grid item style={{ flex: 1 }}>
             <TextField
-              className={classes.input2}
+              style={{ width: '100%' }}
               variant='outlined'
               label='  الترتيب'
               onChange={e => setSort(e.target.value)}
             />
           </Grid>
-          <Grid item>
-            <FormControlLabel
-              control={
-                <Switch
-                  checked={isCompany}
-                  onChange={() => setIsCompany(value => !value)}
-                  name='checkedA'
-                />
-              }
-              label='هل هوا خاص بشركه ؟'
-            />
-          </Grid>
-
-          <Grid container justify='center'>
-            <Grid item>
-              <Button
-                variant='contained'
-                className={classes.button}
-                type='submit'
-              >
-                تم
-              </Button>
-            </Grid>
-          </Grid>
         </Grid>
+        <Grid item style={{ marginTop: '15px' }}>
+          <FormControlLabel
+            control={
+              <Switch
+                checked={isCompany}
+                onChange={() => setIsCompany(value => !value)}
+                name='checkedA'
+              />
+            }
+            label='هل هوا خاص بشركه ؟'
+          />
+        </Grid>
+
+        <Button variant='contained' className={classes.button} type='submit'>
+          انشاء
+        </Button>
       </form>
       <Divider />
-      <Typography variant='h4' color='primary'>
+      <Typography variant='h4' color='primary' style={{ margin: '20px 0px' }}>
         عرض الاصناف الرئسيه
       </Typography>
       <Dialog
         fullWidth={true}
         open={openDialog}
-        onClose={() => setOpenDialog(value => value = false)}
+        onClose={() => setOpenDialog(value => (value = false))}
         aria-labelledby='draggable-dialog-title'
       >
         <DialogTitle style={{ cursor: 'move' }} id='draggable-dialog-title'>
@@ -212,55 +203,61 @@ const AddCatagiories = () => {
           </Typography>
         </DialogTitle>
         <DialogContent>
-          <TextField id="outlined-basic"
-            label="اكتب الأسم الجديد"
-            variant="outlined"
+          <TextField
+            id='outlined-basic'
+            label='اكتب الأسم الجديد'
+            variant='outlined'
             className={classes.dialog}
-            onChange={(text) => {
+            onChange={text => {
               setEditInput(text.target.value)
-            }} />
+            }}
+          />
         </DialogContent>
         <DialogActions>
           <Button
             onClick={() => handleUpdate()}
             variant='contained'
             className={classes.dialog}
-            style={{ margin: '0px 15px', backgroundColor: "#06f", marginBottom: '10px', color: "#FFF" }}
+            style={{
+              margin: '0px 15px',
+              backgroundColor: '#06f',
+              marginBottom: '10px',
+              color: '#FFF'
+            }}
           >
             تعديل
           </Button>
         </DialogActions>
       </Dialog>
-      <Grid container direction='row'>
+      <Grid container direction='row' style={{ gridGap: '6px' }}>
         {catagories.length > 1 && !loading ? (
           catagories.map(catag => (
-            <Card className={classes.card} key={catag._id}>
-              <Grid container justify='space-between'>
-                <Typography variant='h5' className={classes.name}>
-                  {catag.name}
-                </Typography>
-                <Grid item>
-                  <DeleteForeverIcon
-                    className={classes.delIcon}
-                    onClick={() => {
-                      removeOne(catag._id)
-                    }}
-                  />
-
-                  <EditIcon
-                    className={classes.editicon}
-                    onClick={() => {
-                      setSaveId(catag._id)
-                      setOpenDialog(value => value = true)
-                    }}
-                  />
-                </Grid>
-              </Grid>
-            </Card>
+            <Grid item className={classes.cardOfCategories} key={catag._id}>
+              <Typography variant='h5' className={classes.name}>
+                {catag.name}
+              </Typography>
+              <div style={{ display: 'flex', alignItems: 'center' }}>
+                <EditIcon
+                  fontSize='small'
+                  className={classes.editicon}
+                  onClick={() => {
+                    setSaveId(catag._id)
+                    setOpenDialog(value => (value = true))
+                  }}
+                />
+                <DeleteForeverIcon
+                  fontSize='small'
+                  className={classes.delIcon}
+                  onClick={() => {
+                    removeOne(catag._id)
+                  }}
+                />
+              </div>
+            </Grid>
           ))
         ) : (
-            <Animations />
-          )}
+          <Animations />
+        )}
       </Grid>
     </React.Fragment>
   )
--- family-way/src/components/addProducts.js
+++ family-way/src/components/addProducts.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useContext } from 'react'
-import { Grid, TextField, Button } from '@material-ui/core'
+import { Grid, TextField, Button, Divider } from '@material-ui/core'
 import Autocomplete from '@material-ui/lab/Autocomplete'
 import { makeStyles } from '@material-ui/core/styles'
 import { Switch } from '@material-ui/core'
@@ -8,7 +8,7 @@ import DroZone from './DropZone'
 import { productContext } from '../contexts/products/productState'
 import { thirdcatagoriesContext } from '../contexts/thirdcatagories/thirdState'
 import { authContext } from '../contexts/auth/authstate'
-import { Alert } from "@material-ui/lab";
+import { Alert } from '@material-ui/lab'
 
 const useStyles = makeStyles(theme => ({
   field: {
@@ -19,10 +19,7 @@ const useStyles = makeStyles(theme => ({
   },
   button: {
     color: 'white',
-    width: '20em',
-    border: 8,
-    marginTop: '20px',
-    marginLeft: '20px'
+    border: 8
   },
   buttonsubmit: {
     color: 'white',
@@ -33,9 +30,14 @@ const useStyles = makeStyles(theme => ({
     backgroundColor: theme.palette.green.main
   },
   detailsfield: {
-    width: '65em',
-    marginTop: '10px',
-    marginBottom: '10px'
+    width: '100%'
+  },
+  containerTwoColumns: {
+    width: '100%',
+    marginTop: '15px',
+    gridGap: '10px',
+    flexWrap: 'no-wrap',
+    display: 'flex'
   }
 }))
 
@@ -48,7 +50,7 @@ const AddProducts = () => {
   )
   const { addProducts } = useContext(productContext)
 
-  const [alertData, setAlertData] = useState({ open: false });
+  const [alertData, setAlertData] = useState({ open: false })
 
   //const [thirdId,setThirdId]=useState('');
   const [barCode, setBarCode] = useState('')
@@ -120,77 +122,75 @@ const AddProducts = () => {
     if (switchtwo === false) {
       setDiscount(0)
     }
-     if(categories === null){
+    if (categories === null) {
       setAlertData({
         open: true,
-        message: "تاكد من ادخال القسم الثالث",
-        type: "error",
-      });
-    }else if(files.length === 0){
+        message: 'تاكد من ادخال القسم الثالث',
+        type: 'error'
+      })
+    } else if (files.length === 0) {
       setAlertData({
         open: true,
-        message: "تاكد من رفع الصوره  ",
-        type: "error",
-      });
-    } 
-    else if(barCode === ''){
+        message: 'تاكد من رفع الصوره  ',
+        type: 'error'
+      })
+    } else if (barCode === '') {
       setAlertData({
         open: true,
-        message: "تاكد من ادخال الكود بطريقه صحيحه",
-        type: "error",
-      });
-    }else if(title===''){
+        message: 'تاكد من ادخال الكود بطريقه صحيحه',
+        type: 'error'
+      })
+    } else if (title === '') {
       setAlertData({
         open: true,
-        message: "تاكد من ادخال اسم المنتج",
-        type: "error",
-      });
-    }else if (details === '') {
+        message: 'تاكد من ادخال اسم المنتج',
+        type: 'error'
+      })
+    } else if (details === '') {
       setAlertData({
         open: true,
-        message: "تاكد من ادخال تفاصيل المنتج",
-        type: "error",
-      });
-    }else if (price === '') {
+        message: 'تاكد من ادخال تفاصيل المنتج',
+        type: 'error'
+      })
+    } else if (price === '') {
       setAlertData({
         open: true,
-        message: "تاكد من ادخال سعر المنتج",
-        type: "error",
-      });
-    }else if (increaseCount === '') {
+        message: 'تاكد من ادخال سعر المنتج',
+        type: 'error'
+      })
+    } else if (increaseCount === '') {
       setAlertData({
         open: true,
-        message: "تاكد من ادخال مقدار الزياده ",
-        type: "error",
-      });
-    }else if (increaseCount === '') {
+        message: 'تاكد من ادخال مقدار الزياده ',
+        type: 'error'
+      })
+    } else if (increaseCount === '') {
       setAlertData({
         open: true,
-        message: "تاكد من ادخال الوحده ",
-        type: "error",
-      });
-    }else{
+        message: 'تاكد من ادخال الوحده ',
+        type: 'error'
+      })
+    } else {
       addProducts(
-      barCode,
-      files,
-      title,
-      details,
-      categories,
-      price,
-      increaseCount,
-      unit,
-      discount,
-      sold,
-      variationId,
-      discountEnds
-    )
-     setAlertData({
-      open: true,
-      message: "تم اضافه المنتج ",
-      type: "success",
-    });
+        barCode,
+        files,
+        title,
+        details,
+        categories,
+        price,
+        increaseCount,
+        unit,
+        discount,
+        sold,
+        variationId,
+        discountEnds
+      )
+      setAlertData({
+        open: true,
+        message: 'تم اضافه المنتج ',
+        type: 'success'
+      })
     }
-   
   }
 
   return (
@@ -199,79 +199,82 @@ const AddProducts = () => {
         <Alert severity={alertData.type}>{alertData.message}</Alert>
       ) : null}
       <form onSubmit={handleSubmit}>
-        <Grid container direction='column'>
-          <Grid item>
-            {thirdcatagories.length > 0 ? (
-              <Autocomplete
-                className={classes.detailsfield}
-                id='combo-box-demo'
-                onChange={handleFilter}
-                options={thirdcatagories}
-                getOptionLabel={option => option.name}
-                renderInput={params => (
-                  <TextField
-                    {...params}
-                    label='اختر الصنف الثالث'
-                    variant='outlined'
-                  />
-                )}
-              />
-            ) : (
-              <Autocomplete
-                className={classes.detailsfield}
-                id='combo-box-demo'
-                options={text}
-                getOptionLabel={option => option.name}
-                renderInput={params => (
-                  <TextField
-                    {...params}
-                    label='اختر الصنف الثالث'
-                    variant='outlined'
-                  />
-                )}
-              />
-            )}
-          </Grid>
-          <Grid item>
-            <Grid container direction='row'>
-              <Grid item>
+        <Grid container>
+          <Grid item style={{ width: '100%' }}>
+            <Autocomplete
+              className={classes.detailsfield}
+              id='combo-box-demo'
+              onChange={handleFilter}
+              options={thirdcatagories}
+              getOptionLabel={option => option.name}
+              renderInput={params => (
                 <TextField
-                  className={classes.field}
+                  {...params}
+                  label='اختر الصنف الثالث'
                   variant='outlined'
-                  onChange={e => setBarCode(e.target.value)}
-                  label='باركود'
                 />
-              </Grid>
-              <Grid item>
-                <Button
-                  color='primary'
-                  variant='contained'
-                  onClick={SelectFilesButtonHandler}
-                  className={classes.button}
-                >
-                  ادخل الصور
-                </Button>
-              </Grid>
-            </Grid>
+              )}
+            />
           </Grid>
-          <Grid item>
+          <Grid item className={classes.containerTwoColumns}>
             <TextField
               variant='outlined'
-              label='ادخل العنوان'
+              label='اسم المنتج'
               onChange={e => setTitle(e.target.value)}
-              className={classes.detailsfield}
+              style={{ flex: 1 }}
             />
-          </Grid>
-          <Grid item>
             <TextField
               variant='outlined'
-              label='ادخل التفاصيل'
+              label='التفاصيل'
               onChange={e => setDetails(e.target.value)}
-              className={classes.detailsfield}
+              style={{ flex: 1 }}
             />
           </Grid>
-          <Grid item>
+
+          <Grid item className={classes.containerTwoColumns}>
+            <TextField
+              variant='outlined'
+              label='ادخل السعر'
+              onChange={e => setPrice(e.target.value)}
+              style={{ flex: 1 }}
+            />
+            <TextField
+              variant='outlined'
+              label=' مقدار الزياده'
+              onChange={e => setincreaseCount(e.target.value)}
+              style={{ flex: 1 }}
+            />
+            <Autocomplete
+              id='combo-box-demo'
+              options={units}
+              onChange={handleUnit}
+              style={{ flex: 1 }}
+              getOptionLabel={option => option.name}
+              renderInput={params => (
+                <TextField {...params} label='   الوحده' variant='outlined' />
+              )}
+            />
+          </Grid>
+          <Grid item className={classes.containerTwoColumns}>
+            <TextField
+              variant='outlined'
+              onChange={e => setBarCode(e.target.value)}
+              label='باركود'
+              style={{ flex: 1 }}
+            />
+            <Button
+              color='primary'
+              variant='contained'
+              onClick={SelectFilesButtonHandler}
+              className={classes.button}
+              style={{ flex: 1 }}
+            >
+              ادخل الصور
+            </Button>
+          </Grid>
+          <Grid item className={classes.containerTwoColumns}>
             <FormControlLabel
+              style={{ flex: 1 }}
               control={
                 <Switch
                   checked={switchOne}
@@ -282,25 +285,27 @@ const AddProducts = () => {
               label='دمج اكثر من منتج ؟'
             />
           </Grid>
+
           {switchOne ? (
-            <Grid item>
+            <Grid
+              item
+              className={classes.containerTwoColumns}
+              style={{ margin: 0 }}
+            >
               <TextField
+                style={{ flex: 1 }}
                 variant='outlined'
                 label=' اوجه التشابه'
                 onChange={e => setVariationId(e.target.value)}
-                className={classes.detailsfield}
               />
             </Grid>
           ) : null}
-          <Grid item>
-            <TextField
-              variant='outlined'
-              label='ادخل السعر'
-              onChange={e => setPrice(e.target.value)}
-              className={classes.detailsfield}
-            />
-          </Grid>
-          <Grid item>
+
+          <Grid
+            item
+            className={classes.containerTwoColumns}
+            style={{ margin: 0 }}
+          >
             <FormControlLabel
               control={
                 <Switch
@@ -313,37 +318,32 @@ const AddProducts = () => {
             />
           </Grid>
           {switchtwo ? (
-            <Grid>
-              <Grid item>
-                <TextField
-                  variant='outlined'
-                  label=' التخفيض'
-                  onChange={e => setDiscount(e.target.value)}
-                  className={classes.detailsfield}
-                />
-              </Grid>
-              <Grid container justify='center'>
-                <TextField
-                  id='datetime-local'
-                  label='موعد انتهاء التخفيض'
-                  type='datetime-local'
-                  className={classes.textField}
-                  onChange={e => setDiscountEnds(e.target.value)}
-                  InputLabelProps={{
-                    shrink: true
-                  }}
-                />
-              </Grid>
+            <Grid
+              item
+              className={classes.containerTwoColumns}
+              style={{ margin: 5 }}
+            >
+              <TextField
+                style={{ flex: 1 }}
+                variant='outlined'
+                label='قيمة التخفيض'
+                onChange={e => setDiscount(e.target.value)}
+                className={classes.detailsfield}
+              />
+              <TextField
+                style={{ flex: 1 }}
+                id='datetime-local'
+                label='موعد انتهاء التخفيض'
+                type='datetime-local'
+                className={classes.textField}
+                onChange={e => setDiscountEnds(e.target.value)}
+                InputLabelProps={{
+                  shrink: true
+                }}
+              />
             </Grid>
           ) : null}
-          <Grid item>
-            <TextField
-              variant='outlined'
-              label=' مقدار الزياده'
-              onChange={e => setincreaseCount(e.target.value)}
-              className={classes.detailsfield}
-            />
-          </Grid>
+
           {/*
         
         <Grid item>
@@ -354,18 +354,7 @@ const AddProducts = () => {
             className={classes.detailsfield}
           />
         </Grid>*/}
-          <Grid item>
-            <Autocomplete
-              className={classes.detailsfield}
-              id='combo-box-demo'
-              options={units}
-              onChange={handleUnit}
-              getOptionLabel={option => option.name}
-              renderInput={params => (
-                <TextField {...params} label='   الوحده' variant='outlined' />
-              )}
-            />
-          </Grid>
+
           {/* 
         <Grid item>
           <TextField
@@ -385,16 +374,18 @@ const AddProducts = () => {
           />
         </Grid>
         */}
-          <Grid item>
-            <Grid container justify='center'>
-              <Button
-                variant='contained'
-                className={classes.buttonsubmit}
-                type='submit'
-              >
-                تم
-              </Button>
-            </Grid>
+          <Grid item style={{ width: '100%', marginTop: 20 }}>
+            <Button
+              variant='contained'
+              type='submit'
+              style={{
+                width: '100%',
+                backgroundColor: '#30d158',
+                color: '#FFF'
+              }}
+            >
+              تم
+            </Button>
           </Grid>
         </Grid>
       </form>
